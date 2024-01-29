9e985093e23efaffe10f416cda55d17c15600ffb
jeff-hank <146031303+jeff-hank@users.noreply.github.com>
2024-01-28 20:29:48 -0600

    Create server.py

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify
+import socket
+import subprocess
+
+app = Flask(__name__)
+
+@app.get('/')
+def handle_get():
+	hostname = socket.gethostname()
+	address = socket.gethostbyname(hostname)
+    return str(address)
+
+@app.post('/')
+def handle_post():
+	p = subprocess.Popen(["python", "stress_cpu.py"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    return 'success'
